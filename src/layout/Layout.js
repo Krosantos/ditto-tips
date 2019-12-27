87c46fb9b5b76bc90d9a56c5493c4e8ba02fcb31
Tymko Tecca <tymko.tecca@gmail.com>
2019-12-27 00:52:46 -0500

    Fix scroll transitions

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from '@shared/ScrollToTop';
 import Header from './Header';
 import Footer from './Footer';
 import Routes from './Routes';
@@ -8,6 +9,7 @@ import { wrapper } from './layout.scss';
 const Layout = () => (
 	<Router>
 		<Header />
+		<ScrollToTop />
 		<div className={wrapper}>
 			<Routes />
 		</div>
