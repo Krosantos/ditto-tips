9aaef208bcd8fa7202f45eb206fe675a0ee323ca
Tymko Tecca <tymko.tecca@gmail.com>
2019-12-29 10:34:44 -0500

    Ease of use

--- src/pages/groups/EggGroup.js
+++ src/pages/groups/EggGroup.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { kebabCase } from 'lodash';
 import Anchor from '@shared/Anchor';
 import EggRow from './EggRow';
-import { eggGroup } from './groups.scss';
+import { eggGroup, button, header } from './groups.scss';
 
-const EggGroup = ({ group, groupName }) => (
-	<div className={eggGroup}>
-		<Anchor id={kebabCase(groupName)} />
-		<h2>{groupName}</h2>
-		<div>
-			{group.map(({ name, image, eggGroups }) => (
-				<EggRow key={name} eggGroups={eggGroups} image={image} name={name} />
-			))}
+const RETURN = 'Return to Top';
+
+const EggGroup = ({ group, groupName }) => {
+	const returnToTop = useCallback(() => window.scrollTo(0, 0), []);
+
+	return (
+		<div className={eggGroup}>
+			<Anchor id={kebabCase(groupName)} />
+			<div className={header}>
+				<h2>{groupName}</h2>
+				<button className={button} onClick={returnToTop} type="button">{RETURN}</button>
+			</div>
+			<div>
+				{group.map(({ name, image, eggGroups }) => (
+					<EggRow key={name} eggGroups={eggGroups} image={image} name={name} />
+				))}
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default EggGroup;
--- src/pages/groups/index.js
+++ src/pages/groups/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import ByGroups from './ByGroups';
-import { container } from './groups.scss';
+import { container, button, buttonHolder } from './groups.scss';
 
-const EggGroups = () => (
-	<div className={container}>
-		<ByGroups />
-	</div>
-);
+const BY_GROUP = 'By Group';
+const BY_SPECIES = 'By Species';
+
+const EggGroups = () => {
+	const [view, setView] = useState(BY_GROUP);
+	const setByGroup = useCallback(() => {
+		setView(BY_GROUP);
+	}, []);
+	const setBySpecies = useCallback(() => {
+		setView(BY_SPECIES);
+	}, []);
+
+	return (
+		<div className={container}>
+			<div className={buttonHolder}>
+				<button className={button} onClick={setByGroup} type="button">{BY_GROUP}</button>
+				<button className={button} onClick={setBySpecies} type="button">{BY_SPECIES}</button>
+			</div>
+			<ByGroups />
+		</div>
+	);
+};
 
 export default EggGroups;
