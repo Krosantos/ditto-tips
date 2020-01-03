5e4dd07d732dc479b20e9f0ada0384cfea2038ad
Tymko Tecca <tymko.tecca@gmail.com>
2020-01-02 22:12:35 -0500

    Tidy up homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import bigditto from '@images/bigditto.png';
+import { useMediaQuery } from 'react-responsive';
+import { container, bigDitto, centered } from './home.scss';
 
-const HOME = 'Home';
+const TITLE = 'Ditto Tips';
+const BREED = 'breed';
+const TRAIN = 'train';
+const AND = ' and ';
+const COPY_START = 'A sloppy, small website to learn how to ';
+const COPY_END = ' battle-ready pokémon.';
 
-const Home = () => (
-	<div>{HOME}</div>
+const Logo = () => (
+	<div className={bigDitto}>
+		<img alt="" src={bigditto} />
+	</div>
 );
+
+const Home = () => {
+	const isMobile = useMediaQuery({ query: '(max-width : 767px)' });
+
+	return (
+		<div className={container}>
+			{isMobile && <h1>{TITLE}</h1>}
+			<div>
+				<Logo />
+			</div>
+			<p className={centered}>
+				{COPY_START}
+				<a href="/breeding">{BREED}</a>
+				{AND}
+				<a href="/training">{TRAIN}</a>
+				{COPY_END}
+			</p>
+		</div>
+	);
+};
 
 export default Home;
